61f5c57bb990388d3f3b722f780dbb82647ff8b7
Marta Vicente Lezaun <mvicentel@hotmail.com>
2022-12-06 00:10:19 +0100

    close book dialog with a condition

--- app/src/main/java/com/example/mylibrary/view/BookDialog.kt
+++ app/src/main/java/com/example/mylibrary/view/BookDialog.kt
@@ -5,8 +5,10 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import com.example.mylibrary.R
@@ -28,9 +30,14 @@ class BookDialog(private val idBook: Int) : DialogFragment() {
         binding.name.text = data?.name
         binding.author.text = data?.author
         binding.genre.text = getString(R.string.genre).plus(" ").plus(data?.genre)
+        binding.score.text = Editable.Factory.getInstance().newEditable(if(data?.score != 0 ) data?.score.toString() else getString(R.string.score))
 
         binding.checkLoan.isChecked = data?.loan == true
         binding.checkRead.isChecked = data?.read == true
+        binding.checkRead.isEnabled = !binding.checkRead.isChecked
+
+        binding.score.visibility = if(binding.checkRead.isChecked) View.VISIBLE else View.GONE
+        binding.to.visibility = if(binding.checkLoan.isChecked) View.VISIBLE else View.GONE
 
         binding.checkLoan.setOnClickListener { _ ->
             if(binding.checkLoan.isChecked) binding.to.visibility = View.VISIBLE else binding.to.visibility = View.GONE
@@ -42,12 +49,22 @@ class BookDialog(private val idBook: Int) : DialogFragment() {
 
         return AlertDialog.Builder(requireActivity())
             .setView(binding.root)
-            .setPositiveButton(R.string.positive_button, DialogInterface.OnClickListener { _, _ ->
-                val score = binding.score.text.toString().toInt()
-                val lentTo = if(binding.checkLoan.isChecked) binding.to.text.toString() else ""
-                libraryViewModel.saveBook(idBook, binding.checkRead.isChecked, binding.checkLoan.isChecked, score, lentTo)
-                dialog?.cancel()
-            })
+            .setPositiveButton(getString(R.string.positive_button)) { _, _ ->
+                val score =
+                    if (binding.score.text.toString() == getString(R.string.score)) 0 else binding.score.text.toString().toInt()
+                val lentTo =
+                    if (binding.checkLoan.isChecked) if (binding.to.text.toString() == getString(R.string.to)) "" else binding.to.text.toString() else ""
+
+                if ((binding.checkRead.isChecked && (score < 1 || score > 5)) || binding.checkLoan.isChecked && lentTo == "") {
+                    if (binding.checkRead.isChecked) {
+                        binding.score.error = getString(R.string.errorScore)
+                    } else if (binding.checkLoan.isChecked) {
+                        binding.to.error = getString(R.string.errorTo)
+                    }
+                } else {
+                    libraryViewModel.saveBook(idBook, binding.checkRead.isChecked, binding.checkLoan.isChecked, score, lentTo)
+                }
+            }
             .create()
     }
 
